57c8fbdd2b459ae81a557db79ddb2083f304b684
RohitG <rohitgholap045@gmail.com>
2026-05-04 14:49:17 +0530

    added backend hosted url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://blood-donation-network-backend.onrender.com',
 });
 
 // Interceptor to add the JWT token to requests
